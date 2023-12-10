dbdab0383b589e91b8dbe034f56060015a99c433
Rohit Singh <rohitsingh@Rohits-MacBook-Air-2.local>
2023-12-11 00:42:02 +0530

    import fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import UploadForm from "../components/UploadForm";
 import ImageGallery from "../components/ImageGallery";
 
