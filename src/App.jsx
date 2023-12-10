f3598a7dd8b504eb9d811cc8f8d19e9a66435ee4
Rohit Singh <rohitsingh@Rohits-MacBook-Air-2.local>
2023-12-11 00:36:06 +0530

    import fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import { AuthProvider } from "./context/auth";
 import PublicRoute from "./routes/PublicRoute";
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,6 @@ import UploadForm from "../components/UploadForm";
 import ImageGallery from "../components/ImageGallery";
 
 const Home = () => {
-
   return (
     <div className="max-w-4xl mx-auto">
       <Navbar />
